9b0464c4e94c0a82ecc70473b0e9f5391acf3311
17630-90 <caldassamuel21@gmail.com>
2023-11-27 10:22:47 -0300

    sim

--- html.html/pesquisar.js
+++ html.html/pesquisar.js
@@ -1,5 +1,5 @@
 /* JavaScript */
 document.querySelector('.search-icon').addEventListener('click', function() {
     var input = document.querySelector('.search-box input[type="text"]');
-    input.style.display = input.style.display === 'none' ? 'block' : 'none'; // alterna entre mostrar e esconder
+    input.style.opacity = input.style.opacity === '0' ? '1' : '0'; // alterna entre mostrar e esconder
 });
