f14c72bcc350018b5b055b864d8b077666ad666f
17630-90 <caldassamuel21@gmail.com>
2023-11-27 10:30:13 -0300

    sim

--- html.html/pesquisar.js
+++
@@ -1,9 +0,0 @@
-/* JavaScript */
-document.querySelector('.search-icon').addEventListener('click', function() {
-    var input = document.querySelector('#search');
-    if (input.style.display === 'none') {
-        input.style.display = 'block';
-    } else {
-        input.style.display = 'none';
-    }
-});
